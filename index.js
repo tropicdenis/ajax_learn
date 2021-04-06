90e8208f07b3a88cc42564ed7767d4f42a93924a
tropicdenis <tropicdenis@gmail.com>
2021-04-06 22:47:37 +0300

    Ajax 7 is done.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const resultBlock = document.querySelector('#result');
 const pageNumberEl = document.querySelector('#page-number');
-const clickMeButton = document.querySelector('#click-me');
+const GetImagesButton = document.querySelector('#click-me');
+const GetTasksButton = document.querySelector('#get-tasks');
 
-clickMeButton.addEventListener('click', () => {
+GetImagesButton.addEventListener('click', () => {
     const promise = getImages(pageNumberEl.value);
-    promise.then(onDataReceived);
+    promise.then(onImagesReceived);
 });
 
-function onDataReceived(data) {
+GetTasksButton.addEventListener('click', () => {
+    const promise = getTasks();
+    promise.then(onTasksReceived);
+});
+
+function onImagesReceived(data) {
     data.forEach(el => {
         const img = document.createElement('img');
         img.src = el.thumbnail;
@@ -15,3 +21,14 @@ function onDataReceived(data) {
     });
 }
 
+createTask("learnJS").then((data)=> {
+    console.log(data)
+})
+
+function onTasksReceived(tasks) {
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.innerHTML = task.title;
+        document.querySelector('#tasks-result').appendChild(li);
+    });
+}
--- service.js
+++ service.js
@@ -6,13 +6,24 @@
 
 function getImages(pageNumber) {
     const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
-    return promise.then((response) =>{
-        return response.data
-    })
+    return promise.then((response) => {
+        return response.data;
+    });
 }
-function getTasks(pageNumber) {
-    const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
-    return promise.then((response) =>{
+
+function getTasks() {
+    const promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=5341143`);
+    return promise.then((response) => {
+        return response.data;
+    });
+}
+
+function createTask(title) {
+    const promise = axios.post(`https://repetitora.net/api/JS/Tasks`, {
+        widgetId: 5341143,
+        title: title
+    });
+    return promise.then((response) => {
         return response.data
-    })
+    });
 }
