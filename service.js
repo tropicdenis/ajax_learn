2c03cfe685d37ba3ee6ca40225b562dc6f574334
tropicdenis <tropicdenis@gmail.com>
2021-04-06 16:51:39 +0300

    Init

---
+++ service.js
@@ -0,0 +1,18 @@
+/*function getImagesOld(pageNumber) {
+
+    const promise = $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
+    return promise
+}*/
+
+function getImages(pageNumber) {
+    const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
+    return promise.then((response) =>{
+        return response.data
+    })
+}
+function getTasks(pageNumber) {
+    const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
+    return promise.then((response) =>{
+        return response.data
+    })
+}
